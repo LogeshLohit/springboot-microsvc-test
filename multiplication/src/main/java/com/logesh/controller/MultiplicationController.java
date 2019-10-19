dcee585b3b41629f6259d8f139548f1dcbdc1ce5
Logesh <logesh.jeppiaar@gmail.com>
2019-10-19 22:29:33 +0530

    first commit

---
+++ multiplication/src/main/java/com/logesh/controller/MultiplicationController.java
@@ -0,0 +1,38 @@
+package com.logesh.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.logesh.model.MultiplicationGenerator;
+import com.logesh.service.MultiplicationGeneratorServiceImpl;
+
+@RestController
+@CrossOrigin
+public class MultiplicationController {
+
+	@Autowired
+	private MultiplicationGeneratorServiceImpl svc;
+
+	@GetMapping("/getTask")
+	public MultiplicationGenerator generateTask() {
+		return svc.generateTask();
+	}
+
+	@GetMapping("/tasks")
+	public List<MultiplicationGenerator> findAllTasks() {
+		return svc.getAllTasks();
+	}
+
+	@PostMapping("/postResponse")
+	public String getUserResponse(@RequestBody JsonNode obj) {
+		System.out.println("received objet : " + obj.get("users.userName").toString());
+		return "Got it";
+	}
+}
---
+++ multiplication/src/main/java/com/logesh/service/MultiplicationGeneratorServiceImpl.java
@@ -0,0 +1,33 @@
+package com.logesh.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.logesh.model.MultiplicationGenerator;
+import com.logesh.repository.MultiplicationGeneratorRepository;
+
+@Service
+public class MultiplicationGeneratorServiceImpl {
+	@Autowired
+	private MultiplicationGeneratorRepository repo;
+
+	public MultiplicationGenerator generateTask() {
+		int factorA = new Random().nextInt(10) + 1;
+		int factorB = new Random().nextInt(10) + 1;
+		int result = factorA * factorB;
+		MultiplicationGenerator multiplicationTask = new MultiplicationGenerator(factorA, factorB, result);
+		repo.save(multiplicationTask);
+		return multiplicationTask;
+	}
+
+	public List<MultiplicationGenerator> getAllTasks() {
+		// TODO Auto-generated method stub
+		List<MultiplicationGenerator> tasks = new ArrayList<>();
+		repo.findAll().forEach(tasks::add);
+		return tasks;
+	}
+}
