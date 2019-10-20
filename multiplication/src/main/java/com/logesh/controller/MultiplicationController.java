8b1ce4ab52f21c960b8be24c8e734fcdac5d24e8
Logesh <logesh.jeppiaar@gmail.com>
2019-10-20 16:59:58 +0530

    rabbit mq producer and lister in single app

--- multiplication/src/main/java/com/logesh/controller/MultiplicationController.java
+++ multiplication/src/main/java/com/logesh/controller/MultiplicationController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.logesh.model.Attempt;
 import com.logesh.model.MultiplicationGenerator;
 import com.logesh.service.MultiplicationGeneratorServiceImpl;
 
@@ -32,8 +34,15 @@ public class MultiplicationController {
 
 	@PostMapping("/postResponse")
 	public String getUserResponse(@RequestBody JsonNode obj) {
-		System.out.println("received objet : " + obj.get("users.userName").toString());
+		//System.out.println("received objet : " + obj.get("users.userName").toString());
 		svc.saveUserResponse(obj);
 		return "Got it";
 	}
+	
+	@PostMapping("/getAllUserReponseByName")
+	public List<Attempt> getAllUserResponseByName(@RequestBody String name){
+		System.out.println("rq found : "+name);
+		return svc.getAllUserResponseByName(name);
+	}
+	
 }
--- multiplication/src/main/java/com/logesh/service/MultiplicationGeneratorServiceImpl.java
+++ multiplication/src/main/java/com/logesh/service/MultiplicationGeneratorServiceImpl.java
@@ -4,17 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.logesh.model.Attempt;
 import com.logesh.model.MultiplicationGenerator;
+import com.logesh.model.MultiplicationSolvedEvent;
+import com.logesh.model.User;
+import com.logesh.mulitiplication.sender.EventDispatcher;
+import com.logesh.repository.AttemptRepository;
 import com.logesh.repository.MultiplicationGeneratorRepository;
+import com.logesh.repository.UserRepository;
 
 @Service
 public class MultiplicationGeneratorServiceImpl {
 	@Autowired
 	private MultiplicationGeneratorRepository repo;
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private AttemptRepository attemptRepo;
+
+	@Autowired
+	private EventDispatcher eventDispatcher;
 
 	public MultiplicationGenerator generateTask() {
 		int factorA = new Random().nextInt(10) + 1;
@@ -32,8 +49,37 @@ public class MultiplicationGeneratorServiceImpl {
 		return tasks;
 	}
 
+	@Transactional
 	public void saveUserResponse(JsonNode obj) {
 		// TODO Auto-generated method stub
-		
+		User newUser = new User(obj.get("userName").asText());
+		// userRepo.save(newUser);
+		Attempt attempt = new Attempt();
+		attempt.setUsers(newUser);
+		attempt.setUserValue(obj.get("userValue").asInt());
+		MultiplicationGenerator multiplicationTask = repo.findById(obj.get("multiplicationId").asInt()).get();
+		if (multiplicationTask == null) {
+			System.out.println("Unable to find task...sorry!");
+		}
+		attempt.setMultiplicationTask(multiplicationTask);
+		attempt.setCorrect(obj.get("userValue").asInt() == multiplicationTask.getResult());
+		attemptRepo.save(attempt);
+		System.out.println("Attempt saved! :)");
+
+		// SEND EVENT TO MQ
+		MultiplicationSolvedEvent event = new MultiplicationSolvedEvent();
+		event.setAttemptId(attempt.getAttemptId());
+		event.setCorrect(attempt.isCorrect());
+		event.setMultiplicationId(multiplicationTask.getMultiplicationId());
+		event.setUserId(newUser.getUserName());
+		eventDispatcher.send(event);
+		System.out.println("Event sent successfully :)");
+	}
+
+	public List<Attempt> getAllUserResponseByName(String name) {
+		// TODO Auto-generated method stub
+		List<Attempt> userAttempts = new ArrayList<>();
+		attemptRepo.findByUsersUserName(name).forEach(userAttempts::add);
+		return userAttempts;
 	}
 }
